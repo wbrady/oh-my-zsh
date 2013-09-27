92408597e9a9500b2852ec179eaae4d0ca477d62
Will Brady <willgbrady@gmail.com>
2013-09-27 18:02:12 -0400

    Added function to transform movie into gif for pull requests

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -15,3 +15,12 @@ function take() {
   cd $1
 }
 
+gif-ify() {
+  if [[ -n "$1" && -n "$2" ]]; then
+    ffmpeg -i $1 -pix_fmt rgb24 temp.gif
+    convert -layers Optimize temp.gif $2
+    rm temp.gif
+  else
+    echo "proper usage: gif-ify <input_movie.mov> <output_file.gif>. You DO need to include extensions."
+  fi
+}
