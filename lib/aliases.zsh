330243b4c9ca68cdc5d0b502344f295ea52f99a3
Will Brady <willgbrady@gmail.com>
2013-10-31 18:13:00 -0400

    Aliases for work

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -31,3 +31,22 @@ alias bake='be rake'
 alias sub='subl .'
 alias server='open http://localhost:8000 && python -m SimpleHTTPServer'
 
+alias s='echo -n "Please enter your name for screen: " && read && screen -dRR "$REPLY"'
+
+# Taxi Magic
+## Move around
+alias cdrc='cd ~/projects/rc'
+alias cdf='cd ~/projects/fleet-magic'
+alias cdfm='cd ~/projects/fleet-magic'
+alias cdv='cd ~/projects/vehicle'
+alias cdtools='cd ~/projects/devops-tools'
+alias cdchef='cd ~/projects/devops-chef'
+
+## Resque workers
+alias resque_rc_worker="cdrc; bake resque:work QUEUE=* VVERBOSE=1;"
+alias resque_rc_scheduler="cdrc; bake resque:scheduler;"
+alias resque_fm_worker="cdf; bake resque:work QUEUE=* VVERBOSE=1;"
+alias resque_fm_scheduler="cdf; bake resque:scheduler;"
+alias resque_vw_worker="cdv; bake resque:work QUEUE=* VVERBOSE=1;"
+alias resque_vw_scheduler="cdv; bake resque:scheduler;"
+
